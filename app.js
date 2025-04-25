69b8974af1d3f8bb24fa7c541d5de6260a14cc5f
Adriano <gugonavisao@gmail.com>
2025-04-25 19:21:44 -0300

    deus

---
+++ app.js
@@ -0,0 +1,7 @@
+let nome ='Adriano';
+console.log(nome);
+console.log('resultado 1',(4==4)&&(10<5));
+console.log((7!=7)&&(5<4));
+console.log((10 > 8)&&(9!=7));
+console.log((17>=16)&&(14<=15));
+console.log((5>3)||(10 > 5));
