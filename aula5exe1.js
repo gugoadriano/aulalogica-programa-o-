ccdcf8a8179e775b644f4d96f50f014ea75ab818
Adriano <gugonavisao@gmail.com>
2025-04-29 21:22:43 -0300

    oi

---
+++ aula5exe1.js
@@ -0,0 +1,23 @@
+let mes =parseInt(prompt("Digite o número do mês"))
+
+switch(mes){
+    case 12:
+    case 1:
+    case 2:
+        console.log("É UM MÊS DE VERÃO")  
+    break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("É um mês de outono")
+    break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("É um mês de inferno")
+    break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("É primavera")
+}
